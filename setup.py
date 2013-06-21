6bb189886fe0e2bdfa70baee9e1a00d330b26c74
Ian Halpern <ian.halpern@rentshare.com>
2013-06-20 22:12:20 -0400

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(name='streetaddress',
-      version='0.1.2',
+      version='0.1.3',
       description='A Python port of the Perl address parser.',
       author='Mike Jensen',
       url='https://github.com/jjensenmike/python-streetaddress',
       keywords='streetaddress',
       packages=['streetaddress'],
+      install_requires=['datadiff'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Other Environment',
